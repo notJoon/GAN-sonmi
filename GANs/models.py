068e459d62b6bf055c06aaf70d111090e53b6f5f
notJoon <lbj199874@gmail.com>
2022-02-06 20:50:29 +0900

    finished to make gan model

--- GANs/models.py
+++ GANs/models.py
@@ -10,6 +10,8 @@
 # paper:
 ## GAN - https://arxiv.org/pdf/1701.07875.pdf 
 
+import os 
+import pickle as pkl
 import tensorflow
 from tensorflow import keras 
 from tensorflow.keras import Model
@@ -68,7 +70,7 @@ class GAN:
         self.disc_losses = []
         self.gen_losses = []
 
-        self.epochs = 0 
+        self.epoch = 0 
 
         self._build_discriminator()
         self._build_generator()
@@ -213,4 +215,112 @@ class GAN:
 ############# TRAIN METHODS ############# 
 
     def train_discriminator(self, x_train, batch_size, using_generator):
-        ...
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        if using_generator:
+            true_imgs = next(x_train)[0]
+
+            if true_imgs.shape[0] != batch_size:
+                true_imgs = next(x_train)[0]
+        
+        else:
+            idx = np.random.ranint(0, x_train.shape[0], batch_size)
+            true_imgs = x_train[idx]
+        
+        noise = np.random.normal(0, 1, (batch_size, self.z_dim))
+        generate_imgs = self.generator.predict(noise)
+
+        disc_loss_real, disc_acc_real = self.discriminator.train_on_batch(true_imgs, valid)
+        disc_loss_fake, disc_acc_fake = self.discriminator.train_on_batch(generate_imgs, fake)
+        
+        disc_loss = 0.5 * (disc_loss_real + disc_loss_fake)
+        disc_acc = 0.5 * (disc_acc_real + disc_acc_fake)
+
+        return [disc_loss, disc_loss_real, disc_loss_fake, disc_acc, disc_acc_real, disc_acc_fake]
+
+    def train_generator(self, batch_size):
+        valid = np.ones((batch_size, 1))
+        noise = np.random.normal(0, 1, (batch_size, self.z_dim))
+
+        return self.model.train_on_batch(noise, valid)
+    
+    def train(self, x_train, batch_size, epochs, 
+                run_folder, print_every_nth_batches: int=50, using_generator: bool=False):
+        
+        for epoch in range(self.epoch, self.epoch + epochs):
+            disc = self.train_discriminator(x_train, batch_size, using_generator)
+            gen = self.train_generator(batch_size)
+
+            print(f'{epoch} [Discriminator loss: {disc[0]:.3f} (Real: {disc[1]:.3f}, Fake: {disc[2]:.3f}] \
+                [Discriminator acc: ({disc[3]:.3f})({disc[4]:.3f}, {disc[5]:.3f}) \
+                [Generator loss: {gen[0]:.3f}] [Generator acc: {gen[1]:.3f}]')
+        
+            self.disc_losses.append(disc)
+            self.gen_losses.append(gen)
+
+            if epoch % print_every_nth_batches == 0:
+                self.sample_images(run_folder)
+                self.model.save_weights(os.path.join(run_folder, f'weights/weights-{epoch}.h5'))
+                self.model.save_weights(os.path.join(run_folder, f'weights/weights.h5'))
+                self.save_model(run_folder)
+
+            self.epoch += 1
+
+    def sample_images(self, run_folder):
+        rows, cols = 5, 5
+
+        noise = np.random.normal(0, 1, (rows * cols, self.z_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        gen_imgs = 0.5 * (gen_imgs + 1)
+        gen_imgs = np.clip(gen_imgs, 0, 1)
+
+        fig, axis = plt.subplot(rows, cols, figsize=(15, 15))
+        
+        counter = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                axis[i, j].imshow(np.squeeze(gen_imgs[counter, :, :]), cmap='gray')
+                axis[i, j].axis('off')
+                counter += 1
+        
+        fig.savefig(os.path.join(run_folder, f'images/sample_{self.epoch}.png'))
+        plt.close()
+
+
+    def save(self, folder):
+        with open(os.path.join(folder, 'params.pkl'), 'wb') as f:
+            pkl.dump([
+                self.input_dim,
+                self.discriminator_conv_filters,
+                self.discriminator_conv_strides,
+                self.discriminator_batch_norm_momentum,
+                self.discriminator_activation,
+                self.discriminator_dropout_rate,
+                self.discriminator_learning_rate,
+                
+                self.generator_init_dense_layer_size,
+                self.generator_upsample,
+                self.generator_conv_filters,
+                self.generator_conv_kernel_size,
+                self.generator_conv_strides,
+                self.generator_batch_norm_momentum,
+                self.generator_activation,
+                self.generator_dropout_rate,
+                self.generator_learning_rate,
+
+                self.optimizer,
+                self.z_dim
+            ], f)
+
+    def save_model(self, run_folder):
+        self.model.save(os.path.join(run_folder, 'model.h5'))
+        self.discriminator.save(os.path.join(run_folder, 'discriminator.h5'))
+        self.generator.save(os.path.join(run_folder, 'generator.h5'))
+
+        pkl.dump(self.open(os.path.join(run_folder, 'obj.pkl'), 'wb'))
+
+    def load_weights(self, filepath):
+        self.model.load_weights(filepath)
