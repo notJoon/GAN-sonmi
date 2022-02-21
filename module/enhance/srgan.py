9ae0df08452c246303724283a387c83716ac4d79
not_joon <lbj199874@gmail.com>
2022-02-22 08:44:25 +0900

    필요한 부분 추가 함

--- module/enhance/srgan.py
+++ module/enhance/srgan.py
@@ -1,10 +1,122 @@
 ## TODO
 # Finish the SRGAN 
 
+import numpy as np
+import pandas as pd
+import os, math, sys
+import glob, itertools, argparse, random
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from torchvision.models import vgg19
+import torchvision.transforms as transforms 
+from torchvision import datasets
+from torch.utils.data import DataLoader, Dataset
+from torchvision.utils import save_image, make_grid
+
+"""
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+"""
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from tqdm import tqdm_notebook as tqdm
+from sklearn.model_selection import train_test_split
+
+random.seed(42)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+EPOCHS = 2
+BATCH_SIZE = 16
+CHANNELS = 3
+
+load_pretrained_models = True
+PATH = f'../GAN-sonmi/data/clebA'
+
+## for adam
+lr = 0.00008
+b1 = 0.5
+b2 = 0.999
+
+# epoch from which to start lr decay
+decay_epoch = 100
+
+# number of cpu threads to use during batch generation
+#n_cpu = 8
+
+# high res. image height and width
+hr_height, hr_width = 256, 256
+
+
+os.makedirs("images", exist_ok=True)
+os.makedirs("saved_models", exist_ok=True)
+
+cuda = torch.cuda.is_available()
+hr_shape = (hr_height, hr_width)
+
+
+# Normalization parameters for pre-trained PyTorch models
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+
+class ImageDataset(Dataset):
+    def __init__(self, files, hr_shape):
+        hr_height, hr_width = hr_shape
+        # Transforms for low resolution images and high resolution images
+        self.lr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+        self.hr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height, hr_height), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+        self.files = files
+    
+    def __getitem__(self, index):
+        img = Image.open(self.files[index % len(self.files)])
+        img_lr = self.lr_transform(img)
+        img_hr = self.hr_transform(img)
+
+        return {"lr": img_lr, "hr": img_hr}
+
+    def __len__(self):
+        return len(self.files)
+
+train_paths, test_paths = train_test_split(
+        sorted(glob.glob(PATH + "/*.*")), 
+        test_size = 0.02, 
+        random_state = 42
+    )
+
+train_dataloader = DataLoader(
+        ImageDataset(train_paths, hr_shape=hr_shape), 
+        batch_size = BATCH_SIZE, 
+        shuffle = True, 
+        #num_workers = n_cpu
+    )
+
+test_dataloader = DataLoader(
+        ImageDataset(test_paths, hr_shape=hr_shape), 
+        batch_size = int(BATCH_SIZE * 0.75), 
+        shuffle = True, 
+        #num_workers = n_cpu
+    )
+
+
+
 
 class FeatureExtractor(nn.Module):
     def __init__(self):
