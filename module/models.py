8055c7cbe54cd46bb25e711c5f2749f8f809a89f
not_joon <lbj199874@gmail.com>
2022-03-13 10:40:41 +0900

    refactor

--- module/models.py
+++ module/models.py
@@ -7,12 +7,8 @@ import torch.nn as nn
 ## some parsers for control hyperparam
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=150, help="number of epochs of training")
-parser.add_argument('--dropout_rate', type=float, default=0.5, help="set droput rate")
-parser.add_argument('--leakyrelu_rate', type=float, default=0.2, help="set LeakyReLU learning rate")
 parser.add_argument('--z_dim', type=int, default=100, help='generator\'s input noise')
 option = parser.parse_args()
-
-print(option)
 
 """ Architectue
 
@@ -57,54 +53,51 @@ print(option)
 """
 ### DCGAN Layer ###
 class Generator(nn.Module):
-
-    def __init__(self, generator_channels=3, conv_trans_strides=1, 
-                generator_filters=64, kernel_size=4, conv_trans_paddings=0, z_dim=100) -> None:
+    """ DCGAN Generator layer"""
+    def __init__(self, img_channels=1, dim=8, z_dim=100) -> None:
         super(Generator, self).__init__()
+        
+        assert img_channels >= 1, f"img_channel must be 1(greyscale) or 3(RGB). got={img_channels}"
+        assert dim >= 1, f"dim size cannot be zero or negative. the minimum recommend value is 8, got={dim}"
+        assert z_dim >= 100, f"z_dim must greater or equal than 100. got={z_dim}"
 
-        self.generator_channels = generator_channels
-        self.conv_trans_strides = conv_trans_strides
-        self.generator_filters = generator_filters
-        self.kernel_size = kernel_size
-        self.conv_trans_paddings = conv_trans_paddings
+        self.channels = img_channels
         self.z_dim = z_dim 
 
+        def generator_block(self, in_filters:int, out_filters:int, first_block:bool) -> list:
 
-
-        def generator_block(
-            self, in_filters: int, 
-            out_filters: int, 
-            first_block = False) -> list:
             layers = []
-
             layers.append(nn.ConvTranspose2d(
-                in_channels=in_filters,
+                in_channels = in_filters,
+                out_channels = out_filters,
+                kernel_size = 4,
+                stride = 1 if first_block else 2,
+                padding = 0 if first_block else 1
             ))
 
-            layers.append(nn.BatchNorm2d(self.generator_filters))
-            layers.append(nn.ReLU(True))
+            layers.append(nn.BatchNorm2d(out_filters))
+            layers.append(nn.ReLU())
             layers.append(nn.Dropout())
 
             return layers
 
         ### generate layers ###
         layers = []
-        input = self.z_dim
+        in_filters = self.z_dim
 
-        for i, output in enumerate([512, 256, 128, 64]):
+        for i, out_filters in enumerate([dim*8, dim*4, dim*2, dim]):
             layers.extend(generator_block(
                 self,
-                in_filters = input,
-                out_filters = output,
-                first_block = (i == 0)
+                in_filters = in_filters,
+                out_filters = out_filters,
+                first_block = (i==0)
             ))
-
-            input = output
+            in_filters = out_filters
 
         layers.append(nn.ConvTranspose2d(
-            in_channels = input,
-            out_channels = self.generator_channels,
-            kernel_size = self.kernel_size,
+            in_channels = in_filters,
+            out_channels = self.channels,
+            kernel_size = 4,
             stride = 2,
             padding = 1
         ))
@@ -118,59 +111,63 @@ class Generator(nn.Module):
         return self.models(x)
 
 
-
+##TODO 하드코딩 된거 수정하기 
 class Discriminator(nn.Module):
-
-    def __init__(
-        self, discriminator_channels=3, 
-        discriminator_kernels=4, discriminator_strides=2, 
-        discriminator_paddings=1) -> None:
-
+    """ DCGAN Discriminator layer"""
+    def __init__(self, img_channels=1, dim=8) -> None:
         super(Discriminator, self).__init__()
-        self.discriminator_channels = discriminator_channels
-        self.discriminator_kernels = discriminator_kernels
-        self.discriminator_strides = discriminator_strides
-        self.discriminator_paddings = discriminator_paddings
-        self.leaky_relu_rate = 0.2
-        self.dropout_rate = 0.5
+
+        assert img_channels >= 1, f"img_channel size must be 1(greyscale) or 3(RGB). got={img_channels}"
+
+        self.channels = img_channels
 
         def discriminator_block(self, in_filters: int, out_filters: int) -> list:
             layers = []
             layers.append(nn.Conv2d(
                 in_channels = in_filters, 
                 out_channels = out_filters, 
-                kernel_size = self.discriminator_kernels, 
-                stride = self.discriminator_strides, 
-                padding = self.discriminator_paddings, 
+                kernel_size = 4, 
+                stride = 2, 
+                padding = 1, 
                 bias = False))
 
             layers.append(nn.BatchNorm2d(out_filters))
-            layers.append(nn.LeakyReLU(self.leaky_relu_rate, inplace=True))
-            layers.append(nn.Dropout(self.dropout_rate))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(nn.Dropout(0.5))
 
             return layers
 
-        # generate discriminator's layers
-        CONV_INPUT: int = 64
+        # discriminator layers
+        filters: int = 64
 
         layers = []
         layers.append(nn.Conv2d(
-            in_channels = self.discriminator_channels, 
-            out_channels = CONV_INPUT,
-            kernel_size = self.discriminator_kernels,
-            stride = self.discriminator_strides,
-            padding = self.discriminator_paddings))
+            in_channels = self.channels, 
+            out_channels = filters,
+            kernel_size = 4,
+            stride = 2,
+            padding = 1))
         layers.append(nn.Sigmoid())
         
-        input = CONV_INPUT
-        for _, output in enumerate([64, 128, 256, 512]):
+        input = filters
+        for _, output in enumerate([dim, dim*2, dim*4, dim*8]):
             layers.extend(discriminator_block(
                 self, 
                 in_filters = input,
-                out_filters = output))
+                out_filters = output
+            ))
             
-            output = input 
-        
+            input = output
+
+        layers.append(nn.Conv2d(
+            in_channels = output,
+            out_channels = 1,
+            kernel_size = 4,
+            stride = 1,
+            padding = 0
+        ))
+        layers.append(nn.Sigmoid())
+
         self.models = nn.Sequential(*layers)
         print(self.models)
     
@@ -178,61 +175,48 @@ class Discriminator(nn.Module):
         output = self.models(x)
         return output.squeeze()
 
+
+
 ### WGAN-GP Layer ###
 class WGDiscriminator(nn.Module):
-    def __init__(
-        self, conv_kernel_size: int = 4, conv_strides: int = 2, 
-        conv_paddings: int = 1, leaky_rate: float = 0.2, dim: int = 32) -> None:
+    """ WGAN-GP Discriminator layer """
+
+    def __init__(self, img_channels=1, dim=32) -> None:
         super(WGDiscriminator, self).__init__()
-        
-        self.input = 1 
-        self.conv_kernel_size = conv_kernel_size
-        self.conv_strides = conv_strides
-        self.conv_paddings = conv_paddings
-        self.leaky_rate = leaky_rate
-        self.dim = dim
 
-        """ WGAN-GP layer example 
-        Sequential(
-            (0): Conv2d(1, 32, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-            (1): InstanceNorm2d(32, eps=1e-05, momentum=0.1, affine=False, track_running_stats=False)
-            (2): LeakyReLU(negative_slope=0.2)
-            (3): Conv2d(32, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-            (4): InstanceNorm2d(64, eps=1e-05, momentum=0.1, affine=False, track_running_stats=False)
-            (5): LeakyReLU(negative_slope=0.2)
-            (6): Conv2d(64, 128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-            (7): InstanceNorm2d(128, eps=1e-05, momentum=0.1, affine=False, track_running_stats=False)
-            (8): LeakyReLU(negative_slope=0.2)
-            (9): Conv2d(128, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-            (10): InstanceNorm2d(256, eps=1e-05, momentum=0.1, affine=False, track_running_stats=False)
-            (11): LeakyReLU(negative_slope=0.2)
-            (12): Conv2d(256, 1, kernel_size=(4, 4), stride=(1, 1))
-            )
-        """
+        assert img_channels >= 1, f"img_channel size must be 1(greyscale) or 3(RGB). got={img_channels}"
+        assert dim >= 1, f"dim size cannot be zero or negative. the minimum recommend value is 32, got={dim}"
 
-        def discriminator_block(self, in_filters: int, out_filters: int, first_block: bool = False) -> None:
+        self.img_channels = img_channels
+
+        def discriminator_block(self, in_filters:int, out_filters:int, 
+                                first_block:bool=False) -> None:
+
+            assert in_filters > 0, f"in_filters must greater than zero, got={in_filters}"
+            assert out_filters > 0, f"out_filters must greater than zero, got={out_filters}"
+
             layers = []
             layers.append(nn.Conv2d(
                 in_channels = in_filters,
                 out_channels = out_filters,
-                kernel_size = self.conv_kernel_size,
-                stride = self.conv_strides,
-                padding = self.conv_paddings
+                kernel_size = 4,
+                stride = 2,
+                padding = 1
             ))
 
             if first_block:
                 layers.append(nn.InstanceNorm2d(out_filters))
             else:
-                layers.append(nn.InstanceNorm2d(in_filters * 2))
+                layers.append(nn.InstanceNorm2d(in_filters*2))
             
-            layers.append(nn.LeakyReLU(self.leaky_rate))
+            layers.append(nn.LeakyReLU(0.2))
 
             return layers
 
         layers = []
-        in_filters = self.input
+        in_filters = self.img_channels
 
-        for i, out_filters in enumerate([32, 64, 128, 256]):
+        for i, out_filters in enumerate([dim, dim*2, dim*4, dim*8]):
             layers.extend(discriminator_block(
                 self,
                 in_filters = in_filters,
@@ -243,7 +227,7 @@ class WGDiscriminator(nn.Module):
             in_filters = out_filters
 
         layers.append(nn.Conv2d(
-                out_filters, 
+                in_channels = out_filters, 
                 out_channels = 1,
                 kernel_size = 4,
                 stride = 1,
@@ -251,7 +235,14 @@ class WGDiscriminator(nn.Module):
             ))
         
         self.models = nn.Sequential(*layers)
+        
         print(self.models)
 
     def forward(self, x):
         return self.model(x).squeeze()
+
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
--- module/wgangp.py
+++ module/wgangp.py
@@ -1,7 +1,29 @@
+import torch
 import torch.nn as nn 
 import numpy as np
 
 ##TODO implementing gradient penalty and other wgan-gp modules 
 
-def gradient_penalty():
+def get_gradient():
     ... 
+
+def gradient_penalty(gradient) -> torch.Tensor:
+    '''
+    Return the gradient penalty, given a gradient.
+    Given a batch of image gradients, you calculate the magnitude of each image's gradient
+    and penalize the mean quadratic distance of each magnitude to 1.
+    
+    Parameters:
+        * gradient: the gradient of the critic's scores, with respect to the mixed image
+    Returns:
+        * penalty: the gradient penalty
+    '''
+
+    ## flatten the gradients -> each rows captures one image 
+    gradient = gradient.view(len(gradient), -1)
+
+    ## calculate L2-norm (Euclidean distance)
+    gradient_norm = gradient.norm(2, dim=1)
+
+    penalty = torch.mean((gradient_norm-1) ** 2)
+    return penalty 
